bb6605b5d732bf6c4773e446528e9d64dffe2429
JEAN ANDRADE SOARES <jean.asoares@senacsp.edu.br>
2025-03-07 20:10:04 -0300

    Criar Janela

--- main.js
+++ main.js
@@ -1,16 +1,139 @@
-//console.log('Hello Electron')
-
-const { app, BrowserWindow } = require('electron')
-
-const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600
+  // Importação dos recursos do framework
+  // app (aplicação)
+  // BrowserWindow (Criação da janela)
+  // nativeTheme (definir tema claro ou escuro)
+  // Menu (definir um menu personalizado)
+  // shell (acessar links externos no navegador padrão)
+  const { app, BrowserWindow, nativeTheme, Menu, shell } = require('electron/main')
+  
+  // Janela principal
+  let win
+  const createWindow = () => {
+    // definindo tema da janela claro ou escuro
+    nativeTheme.themeSource = 'dark'
+    win = new BrowserWindow({
+      
+      width: 1010,
+      height: 720,
+      //frame: false,
+      //resizable: false,
+      //minimizable: false,
+      //closable: false,
+      //autoHideMenuBar: true
+    })
+  
+    // Carregar o menu personalizado
+    // Atenção! Antes importar o recurso Menu
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template))
+  
+    // carregar o documunte html na janela
+    win.loadFile('./src/views/index.html')
+  }
+  
+  // Janela sobre
+  let about
+  function aboutWindow() {
+    nativeTheme.themeSource='dark'
+    // Obter a janela prinipal
+  const mainWindow = BrowserWindow.getFocusedWindow()
+  // validação (se existir a janela principal)
+  if (mainWindow) {
+    about = new BrowserWindow({
+      width: 320,
+      height: 280,
+      autoHideMenuBar: true,
+      resizable: false,
+      minimizable: false,
+      // estabelecer uma relação hierarquica entre janelas
+      parent: mainWindow,
+      // criar uma janela modal (só retorna a principal quando encerrada)
+      modal: true
+    })
+  }
+    
+    about.loadFile('./src/views/sobre.html')
+  }
+  
+  // inicialização da aplicação (assincronismo)
+  app.whenReady().then(() => {
+    createWindow()
+  
+    // só ativar a janela principal se nenhuma outra estiver ativa
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+      }
+    })
   })
+  
+  // se o sistema não for MAC encerrar a aplicação quando a janela for fechada
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
+  
+  // Reduzir a verbosidade de logs não criticos (devtools)
+  app.commandLine.appendSwitch('log-level','3')
+  
+  // template do menu
+  const template = [
+    {
+      label: 'Cadastro',
+      submenu: [
+        {
+          label: 'Sair',
+          accelerator: 'Alt+F4',
+          click: () => app.quit()
+        }
+      ]
+    },
+    {
+      label: 'Relatório',
+      submenu: [
+        {
+          label: 'Clientes',
+        }
+      ]
+    },
+    {
+      label: 'Ferramentas',
+      submenu: [
+        {
+          label: 'Ampliar zoom',
+          role: 'zoomIn'
+        },
+        {
+          label: 'Reduzir',
+          role: 'zoomOut'
+        },
+        {
+          label: 'Restaurar o zoom padrão',
+          role: 'resetZoom'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'DevTools',
+          role: 'toggleDevTools'
+        }
+      ]
+    },
+    {
+      label: 'Ajuda',
+      submenu: [
+        {
+          label: 'Repositório',
+          click: () => shell.openExternal('https://github.com/Jean-Soares9/cadastro.git')
+        },
+        {
+          label: 'Sobre',
+          click: () => aboutWindow()
+        }
+      ]
+    }
+  ]
+  
+  
 
-  win.loadFile('index.html')
-}
-
-app.whenReady().then(() => {
-  createWindow()
-})
